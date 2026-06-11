8fb8422792a7f748131698cecc9471f128a2b81c
Anton Danilyuk <danilyuk.anton2023@mail.ru>
2026-06-11 15:19:49 +0300

    create migrations for user and specialization models, create disease and drug models, create migrations for disease and drug models

---
+++ src/app/models/drugs.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped
+
+from core.models import BaseModel
+from core import orm as orm_fields
+
+class Drugs(BaseModel):
+    __tablename__ = 'drugs'
+
+    name: Mapped[orm_fields.unique_str_128]
+
+    description: Mapped[orm_fields.text_column]
